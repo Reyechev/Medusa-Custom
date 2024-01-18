ac2aacf09f244632f635425ed743b2a8ac9035dc
PlatypusAlpha <eddycarra@platypuscyberpunks.com>
2024-01-18 02:21:10 -0500

    Corrected medusa_config file

--- burrow/medusa-config.js
+++ burrow/medusa-config.js
@@ -84,7 +84,6 @@ const projectConfig = {
   admin_cors: ADMIN_CORS,
   database_url: DATABASE_URL,
   database_extra: { ssl: { rejectUnauthorized: false } },
-  admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
 };
