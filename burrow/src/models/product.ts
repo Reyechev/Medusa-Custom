e1269c1daecc3897e1462dfbae11aa41db8e1538
Golddy Dev <firingdev@gmail.com>
2024-01-19 17:51:52 +0900

    fix the cors issue on route

--- burrow/src/models/product.ts
+++ burrow/src/models/product.ts
@@ -1,7 +1,5 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm"
-import {
-  Product as MedusaProduct,
-} from "@medusajs/medusa"
+import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
+import { Product as MedusaProduct } from "@medusajs/medusa";
 import { Store } from "./store";
 
 @Entity()
@@ -11,6 +9,6 @@ export class Product extends MedusaProduct {
   store_id?: string;
 
   @ManyToOne(() => Store, (store) => store.products)
-  @JoinColumn({ name: 'store_id', referencedColumnName: 'id' })
+  @JoinColumn({ name: "store_id", referencedColumnName: "id" })
   store?: Store;
 }
