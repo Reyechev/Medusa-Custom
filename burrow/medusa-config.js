d64168494abbd4389d4518ed9cfe3b4a963c0747
PlatypusAlpha <eddycarra@platypuscyberpunks.com>
2024-02-01 22:57:37 -0500

    env file and medusa-config

--- burrow/medusa-config.js
+++ burrow/medusa-config.js
@@ -34,14 +34,10 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_DATABASE = process.env.DB_DATABASE;
-const DB_STRING = process.env.DB_STRING;
 
-
-  const DATABASE_URL = DB_STRING;
-  // const DATABASE_URL =
-  // `postgresql://${DB_USERNAME}:${DB_PASSWORD}` +
-  // `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-
+const DATABASE_URL =
+  `postgresql://${DB_USERNAME}:${DB_PASSWORD}` +
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 // REDIS
 const REDIS_USERNAME = process.env.REDIS_USERNAME;
