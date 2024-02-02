522ec4366daa6da25a5fb1c7cab97bf55141647b
PlatypusAlpha <eddycarra@platypuscyberpunks.com>
2024-02-01 22:49:39 -0500

    env file and medusa-config

--- burrow/medusa-config.js
+++ burrow/medusa-config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
   case "production":
-    ENV_FILE_NAME = ".env";
+    ENV_FILE_NAME = ".env.production";
     break;
   case "staging":
     ENV_FILE_NAME = ".env.staging";
@@ -34,10 +34,14 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_DATABASE = process.env.DB_DATABASE;
+const DB_STRING = process.env.DB_STRING;
 
-const DATABASE_URL =
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+  const DATABASE_URL = DB_STRING;
+  // const DATABASE_URL =
+  // `postgresql://${DB_USERNAME}:${DB_PASSWORD}` +
+  // `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
 
 // REDIS
 const REDIS_USERNAME = process.env.REDIS_USERNAME;
